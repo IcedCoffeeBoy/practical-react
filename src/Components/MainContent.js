67447ea797b5cf659dc0db8b605ed9688481ef2f
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-01-23 15:32:41 +0800

    Added forms

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react'
 
 class MainContent extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { namevalue: '', yearvalue: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const name = event.target.name
+        this.setState({ [name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('The following will be submitted:\n' +
+            'Name:' + this.state.namevalue + '\n' +
+            'Year:' + this.state.yearvalue);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <ul>
-                <li>Name:
-                    <input type='text'></input>
-                </li>
-                <li>Faculty:
-                    <input type='text'></input>
-                </li>
-                <li>Etc:
-                    <input type='text'></input>
-                </li>
-            </ul>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name: <input type="text" name='namevalue' value={this.state.namevalue} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Year: <input type="number" name='yearvalue' value={this.state.yearvalue} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 
